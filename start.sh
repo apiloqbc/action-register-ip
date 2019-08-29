3fd390e96f474e7a4bf4ac8aca3976e2ea21cb69
Andrea, Pilo <apilo@cuebiq.com>
2019-08-29 16:29:40 +0200

    Trigger build

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "Push to branch ${SSH_KEY}";
-[ -z "${SSH_KEY}" ] && {
+echo "Push to branch ${repo-token}";
+[ -z "${repo-token}" ] && {
     echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
     exit 1;
 };
 
-header=$(echo -n "ad-m:${SSH_KEY}" | base64)
+header=$(echo -n "ad-m:${repo-token}" | base64)
 git -c http.extraheader="AUTHORIZATION: basic $header" push origin refs/remotes/origin/master:master;
